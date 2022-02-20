3b367c95d0476e08082adab4a89adfb0d17e010b
Norbert Horgas <norbert.horgas@mathema.de>
2022-02-20 11:22:08 +0100

    Add config for ios, env variables

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.horbee.ionic-react-news",
-  appName: "ionic-react-news",
-  webDir: "build",
+  appId: 'com.horbee.reactnews',
+  appName: 'ionic-react-news',
+  webDir: 'build',
   bundledWebRuntime: false,
-};
+}
 
-export default config;
+export default config
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,12 +1,7 @@
 import {
-  basketballOutline,
-  happyOutline,
-  hardwareChipOutline,
-  heartOutline,
-  homeOutline,
-  schoolOutline,
-  walletOutline,
-} from 'ionicons/icons'
+    basketballOutline, happyOutline, hardwareChipOutline, heartOutline, homeOutline, schoolOutline,
+    walletOutline
+} from "ionicons/icons";
 
 // API KEY for news API
 export const API_KEY = process.env.REACT_APP_NEWS_API_KEY ?? ''
@@ -28,6 +23,7 @@ export const categoryOptions: CategoryOption[] = [
   { id: 'technology', name: 'Tech', icon: hardwareChipOutline },
 ]
 
-export const newsEndpoints = 'https://newsapi.org/v2/top-headlines'
+export const newsEndpoints =
+  process.env.REACT_APP_NEWS_API ?? 'https://newsapi.org/v2/top-headlines'
 export const rssConverterEndpoints =
   process.env.REACT_APP_RSS_CONVERTER_API ?? 'http://localhost:8080/api/feed'
