fed98339ad59296b5e62bf719897c761257a96b8
Norbee <n.horox@gmail.com>
2022-02-24 11:52:58 +0100

    React to RSS list changes

--- src/hooks/useRSSChips.ts
+++ src/hooks/useRSSChips.ts
@@ -1,13 +1,16 @@
-import { useContext, useState } from 'react'
-import { RSSFeedContext } from '../context/RSSFeedContext'
-import { RSSFeedAddress } from '../models/rss-feed-data'
+import { useContext, useEffect, useState } from "react";
+
+import { RSSFeedContext } from "../context/RSSFeedContext";
+import { RSSFeedAddress } from "../models/rss-feed-data";
 
 export const useRSSChips = () => {
   const { rssAddressList } = useContext(RSSFeedContext)
 
-  const [selectedFeed, setSelectedFeed] = useState<RSSFeedAddress | undefined>(
-    rssAddressList[0]
-  )
+  const [selectedFeed, setSelectedFeed] = useState<RSSFeedAddress | undefined>()
+
+  useEffect(() => {
+    setSelectedFeed(rssAddressList[0])
+  }, [rssAddressList])
 
   const isSelected = (feed: RSSFeedAddress) => feed.id === selectedFeed?.id
 
