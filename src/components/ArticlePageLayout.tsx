a1a1aff63a4fddaea7e7915943665781905e8bcf
Norbee <n.horox@gmail.com>
2022-02-24 12:53:46 +0100

    Add scroll to top feature if navigate to same route

--- src/components/ArticlePageLayout.tsx
+++ src/components/ArticlePageLayout.tsx
@@ -1,20 +1,10 @@
-import { logoRss, settingsOutline } from 'ionicons/icons'
-import { FC } from 'react'
+import { logoRss, settingsOutline } from "ionicons/icons";
+import { FC, useEffect, useRef } from "react";
 
 import {
-  IonButton,
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonPage,
-  IonRefresher,
-  IonRefresherContent,
-  IonTitle,
-  IonToolbar,
-  RefresherEventDetail,
-  useIonRouter,
-} from '@ionic/react'
+    IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonRefresher,
+    IonRefresherContent, IonTitle, IonToolbar, RefresherEventDetail, useIonRouter
+} from "@ionic/react";
 
 interface ArticlePageLayoutProps {
   title: string
@@ -27,13 +17,34 @@ export const ArticlePageLayout: FC<ArticlePageLayoutProps> = ({
   refreshFunction,
 }) => {
   const router = useIonRouter()
+  const contentRef = useRef<HTMLIonContentElement>(null)
+
+  useEffect(() => {
+    const handleScrollToTop = (e: any) => {
+      if (e.detail.selected) {
+        contentRef.current?.scrollToTop(500)
+      }
+    }
+    window.addEventListener('ionTabButtonClick', handleScrollToTop)
+
+    return () => {
+      window.removeEventListener('ionTabButtonClick', handleScrollToTop)
+    }
+  }, [])
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="end">
-            <IonButton onClick={() => router.push('/rss')}>
+            <IonButton
+              onClick={() => {
+                router.push('/rss')
+                if (router.routeInfo.pathname === '/rss') {
+                  contentRef.current?.scrollToTop(500)
+                }
+              }}
+            >
               <IonIcon icon={logoRss}></IonIcon>
             </IonButton>
             <IonButton onClick={() => router.push('/options')}>
@@ -43,7 +54,7 @@ export const ArticlePageLayout: FC<ArticlePageLayoutProps> = ({
           <IonTitle>{title}</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen ref={contentRef}>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">{title}</IonTitle>
